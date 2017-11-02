f36606ce24e1eed5bc3d3cfada860bb380b09a7d
es021 <zulsarhan.shaari@gmail.com>
2017-11-02 17:28:03 +0800

    add user page pagination

--- src/redux/actions/user-actions.js
+++ src/redux/actions/user-actions.js
@@ -3,13 +3,16 @@ import {getAxiosGraphQLQuery} from './_helper-actions';
 
 export const FETCH_USER = "FETCH_USER";
 
-export function loadUser() {
+export function loadUser(page) {
+    var offset = 10;
+    console.log("load page ",page);
     return function (dispatch) {
         dispatch({
             type: FETCH_USER,
             payload: getAxiosGraphQLQuery(axios
                     , `query{
-                            users(role:"rec"){
+                            users(role:"student", page:${page}, offset:${offset}){
+                                ID
                                 first_name
                                 last_name
                             }
